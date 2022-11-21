2d6a2f3d6139bcc1762b28e6bb225ebcae43357a
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-11-21 20:03:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
 
 // Paypal
 
- function initPayPalButton() {
-    paypal.Buttons({
-      style: {
-        shape: 'rect',
-        color: 'gold',
-        layout: 'vertical',
-        label: 'paypal',
-        
-      },
-
-      createOrder: function(data, actions) {
-        return actions.order.create({
-          purchase_units: [{"amount":{"currency_code":"USD","value":2.46}}]
-        });
-      },
-
-      onApprove: function(data, actions) {
-        return actions.order.capture().then(function(orderData) {
-          
-          // Full available details
-          console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-          // Show a success message within this page, e.g.
-          const element = document.getElementById('paypal-button-container');
-          element.innerHTML = '';
-          element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-          // Or go to another URL:  actions.redirect('thank_you.html');
-          
-        });
-      },
-
-      onError: function(err) {
-        console.log(err);
-      }
-    }).render('#paypal-button-container');
+ paypal.Buttons({
+  // Sets up the transaction when a payment button is clicked
+  createOrder: (data, actions) => {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '2.5' // Can also reference a variable or function
+        }
+      }]
+    });
+  },
+  // Finalize the transaction after payer approval
+  onApprove: (data, actions) => {
+    return actions.order.capture().then(function(orderData) {
+      // Successful capture! For dev/demo purposes:
+      console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+      const transaction = orderData.purchase_units[0].payments.captures[0];
+      alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+      // When ready to go live, remove the alert and show a success message within this page. For example:
+      // const element = document.getElementById('paypal-button-container');
+      // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+      // Or go to another URL:  actions.redirect('thank_you.html');
+    });
   }
-  initPayPalButton();
-
+}).render('#paypal-button-container');
 
 //Photo
 
